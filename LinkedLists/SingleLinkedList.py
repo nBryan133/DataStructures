e58c4ce397455ce541977dad9988267a9bfb4bed
nBryan <nbryan133@hotmail.com>
2025-03-28 10:31:10 -0400

    added searching for presence of value in list

--- LinkedLists/SingleLinkedList.py
+++ LinkedLists/SingleLinkedList.py
@@ -5,9 +5,45 @@ class Node:
         self.next = None           #pointer to next node
         
 #function to print data via traversing linked list iteratively
-def iTraverse(head):
+#time complexity: O(n)
+#space complexity: O(1)
+def iTrav(head):
+    
+    cur = head
     
     #while not through full list print data of each node
-    while head is not None:
+    while cur != None:
+        print(cur.data)
+        cur = cur.next
+
+#function to print data via recursive function
+#time complexity: O(n)
+#space complexity: O(n)
+def rTrav(head):
+    if head == None:
+        return head
+    else:
         print(head.data)
-        head = head.next
+        rTrav(head.next)
+        
+#function to find element in linked list or lack there of iteratively
+#time complexity: O(n)
+#space complexity: O(1)
+def iSearch(head, target):
+    cur = head
+    
+    while cur != None:
+        if cur.data == target:
+            return True
+        cur = cur.next
+    return False
+
+#function to find element in linked list or lack there of recursively
+#time complexity: O(n)
+#space complexity: O(n)
+def rSearch(head, target):
+    if(head == None):
+        return False
+    elif(head.data == target):
+        return True
+    return rSearch(head.next, target)
--- LinkedLists/testbed.py
+++ LinkedLists/testbed.py
@@ -6,4 +6,25 @@ head.next = Node(2)
 head.next.next = Node(3)
 head.next.next.next = Node(4)
 
-SingleLinkedList.iTraverse(head)
+#SingleLinkedList.iTrav(head)
+#SingleLinkedList.rTrav(head)
+
+#if(SingleLinkedList.iSearch(head, 3)):
+#    print("target found")
+#else:
+#    print ("target not found")
+    
+#if(SingleLinkedList.iSearch(head, 6)):
+#    print("target found")
+#else:
+#    print ("target not found")
+
+#if(SingleLinkedList.rSearch(head, 3)):
+#    print("target found")
+#else:
+#    print ("target not found")
+    
+#if(SingleLinkedList.rSearch(head, 6)):
+#    print("target found")
+#else:
+#    print ("target not found")
